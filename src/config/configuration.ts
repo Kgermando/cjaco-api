13682806422563ab3f99605734a0db0cac3acecb
Kgermando <katakugermain@gmail.com>
2023-06-20 16:03:02 +0100

    dd

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,12 +6,6 @@ export default () => ({
     database: {
       url: isProduction ? process.env.DATABASE_URL
         : `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASS}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`
-         
-      // host: process.env.POSTGRES_HOST,
-      // port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      // user: process.env.POSTGRES_USER,
-      // password: process.env.POSTGRES_PASS,
-      // db: process.env.POSTGRES_DATABASE,
     },
   }
 );
