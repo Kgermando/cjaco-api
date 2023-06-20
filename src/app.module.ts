964c07bc8e03db578e86235b574673e797925093
Kgermando <katakugermain@gmail.com>
2023-06-20 17:36:46 +0100

    kk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
@@ -35,7 +33,5 @@ const isProduction = process.env.NODE_ENV === "production";
     AuthModule,
     
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
