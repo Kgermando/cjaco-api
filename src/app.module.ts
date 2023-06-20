fb6e51af90ba6044ec5328ca8bdb5b9fa7e3fdce
Kgermando <katakugermain@gmail.com>
2023-06-20 16:02:48 +0100

    production

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,12 @@ import configuration from './config/configuration';
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.user'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.db'), 
+        url: configService.get<string>('database.url'),
+        // host: configService.get<string>('database.host'),
+        // port: configService.get<number>('database.port'),
+        // username: configService.get<string>('database.user'),
+        // password: configService.get<string>('database.password'),
+        // database: configService.get<string>('database.db'), 
         autoLoadEntities: true,
         synchronize: true,
       }), 
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,12 +1,18 @@
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
-      // url: process.env.DATABASE_URL,
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASS,
-      db: process.env.POSTGRES_DATABASE,
+      url: isProduction ? process.env.DATABASE_URL
+        : `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASS}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`
+         
+      // host: process.env.POSTGRES_HOST,
+      // port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      // user: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASS,
+      // db: process.env.POSTGRES_DATABASE,
     },
   }
 );
+ 
