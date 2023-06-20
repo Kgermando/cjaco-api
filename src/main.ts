17cc1349a7cea99001ffae4ee3a6718d76d386b0
Kgermando <katakugermain@gmail.com>
2023-06-20 12:54:02 +0100

    dd

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000); 
+  // await app.listen(process.env.PORT || 8080); 
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
 }
 bootstrap();
 
