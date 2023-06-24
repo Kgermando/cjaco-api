4380a514c48cd6e6e8f11e3e68c79f8e427eddba
Kgermando <katakugermain@gmail.com>
2023-06-24 19:19:31 +0100

    ssqaaaxxxxxxx

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -22,7 +22,7 @@ export class ImageService {
     private bucket = this.config.get<string>('IMAGE_S3_BUCKET');
     private region = this.config.get<string>('IMAGE_S3_REGION');
     private basePath = process.cwd();
-    private publicBucketUrl = `https://${this.bucket}.${this.region}.amazonaws.com`; 
+    private publicBucketUrl = `https://${this.bucket}.${this.region}.amazonaws.com`;
 
     async handleImage(file: Express.Multer.File) {
         const bucketFileName = file.filename + extname(file.originalname);
@@ -42,7 +42,7 @@ export class ImageService {
               Key: bucketFileName,
               Body: await readFile(localPath),
               Metadata: {
-                'Content-Type': file.mimetype,
+                'Content-Type': 'image/jpeg',
               },
             }),
           );
